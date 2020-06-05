ac45c7635409afeecb76a6746c195e750525fcf2
Marcin1665 <marcin_tuszynski@hotmail.com>
2020-06-05 19:56:31 +0200

    fix

--- app/src/main/java/com/example/quotegenerator/MainActivity.java
+++ app/src/main/java/com/example/quotegenerator/MainActivity.java
@@ -1,7 +1,5 @@
 package com.example.quotegenerator;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
@@ -9,19 +7,24 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.ArrayList;
-import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    public TextView quotes_textview;
-    public Button generate;
-    public Button share;
-    public ArrayList<Quote> quoteArrayList;
-    public int index;
+    private TextView forMarcin;
+    private TextView forJustyna;
+    private TextView forEdyta;
+    private Button generate;
+    private Button marcin;
+    private Button justyna;
+    private Button edyta;
+    private ArrayList<Quote> quoteArrayList;
+    private int index;
 
     public void addToQuoteArrayList(String[] allQuotes, String[] allAuthors) {
-        for(int i = 0;i <allQuotes.length - 1;i++){
+        for (int i = 0; i < allQuotes.length - 1; i++) {
             String quote = allQuotes[i];
             String author = allAuthors[i];
             Quote newQuote = new Quote(quote, author);
@@ -30,10 +33,9 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public int getRandomQuote(int length){
+    public int getRandomQuote(int length) {
         return (int) (Math.random() * length) + 1;
     }
-
 
 
     @Override
@@ -41,41 +43,58 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        quotes_textview = (TextView) findViewById(R.id.quotes_textview);
-        generate = (Button) findViewById(R.id.generate);
-        share = (Button) findViewById(R.id.share);
+
+        marcin = (Button) findViewById(R.id.marcin);
+        justyna = (Button) findViewById(R.id.justyna);
+        edyta = (Button) findViewById(R.id.edyta);
+
+        forMarcin = (TextView) findViewById(R.id.forMarcin);
+        forJustyna = (TextView) findViewById(R.id.forJustyna);
+        forEdyta = (TextView) findViewById(R.id.forEdyta);
+
 
         Resources resources = getResources();
         String[] allQuotes = resources.getStringArray(R.array.quotes);
         String[] allAuthors = resources.getStringArray(R.array.authors);
-        quoteArrayList = new ArrayList<>();;
+        quoteArrayList = new ArrayList<>();
+
         addToQuoteArrayList(allQuotes, allAuthors);
 
         final int quotesLength = quoteArrayList.size();
 
         index = getRandomQuote(quotesLength - 1);
 
-        quotes_textview.setText(quoteArrayList.get(index).toString());
-
-        generate.setOnClickListener(new View.OnClickListener() {
+        marcin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 index = getRandomQuote(quotesLength - 1);
-                quotes_textview.setText(quoteArrayList.get(index).toString());
+                forMarcin.setText(quoteArrayList.get(index).toString());
+                if (forMarcin.getText() == forJustyna.getText() || forMarcin.getText() == forEdyta.getText()){
+                    forMarcin.setText(quoteArrayList.get(index).toString());
+                }
             }
         });
-
-        share.setOnClickListener(new View.OnClickListener() {
+        justyna.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent sendIntent = new Intent();
-                sendIntent.setAction(Intent.ACTION_SEND);
-                sendIntent.putExtra(Intent.EXTRA_TEXT, quoteArrayList.get(index).toString());
-                sendIntent.setType("text/plain");
-                startActivity(sendIntent);
-
+                index = getRandomQuote(quotesLength - 1);
+                forJustyna.setText(quoteArrayList.get(index).toString());
+                if (forMarcin.getText() == forJustyna.getText() || forJustyna.getText() == forEdyta.getText()){
+                    forJustyna.setText(quoteArrayList.get(index).toString());
+                }
             }
         });
+        edyta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                index = getRandomQuote(quotesLength - 1);
+                forEdyta.setText(quoteArrayList.get(index).toString());
+                if (forEdyta.getText() == forJustyna.getText() || forMarcin.getText() == forEdyta.getText()){
+                    forEdyta.setText(quoteArrayList.get(index).toString());
+                }
+            }
+        });
+
 
 
 
